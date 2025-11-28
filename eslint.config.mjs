4be3fbdde798126dba4e73e76c2dc5ded9f65d60
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-28 13:24:11 +0800

    chore: turn off "import-x/prefer-default-export"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,7 @@ const jsConfig = [
     rules: {
       // Disable Import X order rules to avoid conflicts with `@ianvs/prettier-plugin-sort-imports`
       "import-x/order": "off",
+      "import-x/prefer-default-export": "off",
     },
   },
 ];
