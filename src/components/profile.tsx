f42704d375f6166605bf23fb1e3b712a8cf7103c
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-28 21:36:38 +0800

    feat: add avatars

---
+++ src/components/profile.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+
+import type { AvatarProps } from "./ui/avatar";
+
+import Ahmad from "../app/avatars/ahmad.jpg";
+import Alliyah from "../app/avatars/alliyah.jpg";
+import Jessa from "../app/avatars/jessa.jpg";
+import Jhujane from "../app/avatars/jhujane.jpg";
+import Joaquin from "../app/avatars/joaquin.jpg";
+import Razel from "../app/avatars/razel.jpg";
+import Tal from "../app/avatars/tal.jpg";
+import { Avatar } from "./ui/avatar";
+
+type PersonName = "ahmad" | "alliyah" | "jessa" | "jhujane" | "joaquin" | "razel" | "tal";
+
+const PEOPLE_MAP: Record<PersonName, { image: string; initials: string; name: string }> = {
+  ahmad: { image: Ahmad.src, initials: "AP", name: "Ahmad Panday" },
+  alliyah: { image: Alliyah.src, initials: "AL", name: "Alliyah Daphne Lao" },
+  jessa: { image: Jessa.src, initials: "JE", name: "Jessamea Cañada" },
+  jhujane: { image: Jhujane.src, initials: "JH", name: "Jhujane Pante" },
+  joaquin: { image: Joaquin.src, initials: "JO", name: "John Joaquin Valdez" },
+  razel: { image: Razel.src, initials: "RA", name: "Rirginia Razel Sabado" },
+  tal: { image: Tal.src, initials: "TA", name: "Christal Sofia Malbago" },
+};
+
+interface ProfilesProps {
+  people: PersonName[];
+}
+
+export function Profile({ people, size = "2xl", className = "" }: AvatarProps & ProfilesProps) {
+  return (
+    <div className={cn("flex items-center -space-x-4", className)}>
+      {people.map((person) => {
+        const data = PEOPLE_MAP[person];
+        return (
+          <Avatar
+            className="not-prose ring-2 ring-white dark:ring-zinc-900"
+            src={data.image}
+            alt={data.name}
+            initials={data.initials}
+            key={person}
+            size={size}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
+          />
+        );
+      })}
+    </div>
+  );
+}
