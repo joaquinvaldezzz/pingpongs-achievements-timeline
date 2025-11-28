907f5def8eb32ddb2a5ee558233933f540b8f3c9
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-28 21:56:59 +0800

    feat: update positions and markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,23 @@ export default async function Page() {
         {achievements.map((achievement, index) => (
           <li
             className={cn(
-              "relative grid grid-cols-1 items-baseline gap-2 pl-8 not-last:pb-6 md:grid-cols-3 md:gap-4 md:pl-10",
-              "before:absolute before:top-0.5 before:left-[calc(--spacing(1)-1px)] before:size-4 before:rounded-full before:bg-gray-200 md:before:top-2.5",
-              "after:absolute after:top-1 after:bottom-0 after:left-2.75 after:w-px after:bg-gray-200 md:after:top-0",
+              "relative grid grid-cols-1 items-baseline gap-2 pl-10 not-last:pb-6 md:grid-cols-3 md:gap-4",
+              "before:absolute before:top-0.5 before:left-2.5 before:size-4 before:rounded-full before:bg-gray-200 md:before:top-2.5",
+              "after:absolute after:top-1 after:bottom-0 after:left-4.5 after:w-px after:bg-gray-200 md:after:top-0",
             )}
             key={`achievement-${index + 1}`}
           >
             <div className="md:col-span-1">
-              <p className="text-sm text-gray-600">
-                <time dateTime={achievement.title}>{formatDate(achievement.title)}</time>
-              </p>
+              <h2 className="text-sm text-gray-600">
+                <time className="slashed-zero" dateTime={achievement.title}>
+                  {formatDate(achievement.title)}
+                </time>
+              </h2>
             </div>
 
-            <div className="prose md:col-span-2">{achievement.content}</div>
+            <div className="prose md:col-span-2 dark:prose-invert prose-h3:tracking-tight">
+              {achievement.content}
+            </div>
           </li>
         ))}
       </ol>
