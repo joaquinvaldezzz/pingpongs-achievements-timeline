f2d8cb0869e372c876e4f85adcf18dd7299de545
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-28 13:00:12 +0800

    feat: add MDX integration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+import createMDX from "@next/mdx";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   reactCompiler: true,
   typedRoutes: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
