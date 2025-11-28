3274c2a4af6ebd3d94aed69e33f84bc02c4c3b1e
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-28 12:56:08 +0800

    style: format using Prettier

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -23,8 +23,10 @@ import {
 import { fieldStyles } from "./field";
 import { PopoverContent } from "./popover";
 
-interface SelectProps<T extends object, M extends "single" | "multiple" = "single">
-  extends SelectPrimitiveProps<T, M> {
+interface SelectProps<
+  T extends object,
+  M extends "single" | "multiple" = "single",
+> extends SelectPrimitiveProps<T, M> {
   items?: Iterable<T, M>;
 }
 
@@ -41,8 +43,10 @@ function Select<T extends object, M extends "single" | "multiple" = "single">({
   );
 }
 
-interface SelectListProps<T extends object>
-  extends Omit<ListBoxProps<T>, "layout" | "orientation"> {
+interface SelectListProps<T extends object> extends Omit<
+  ListBoxProps<T>,
+  "layout" | "orientation"
+> {
   items?: Iterable<T>;
   popover?: Omit<PopoverProps, "children">;
 }
