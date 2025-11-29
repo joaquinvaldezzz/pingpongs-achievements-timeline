42be234d0ad054139f64aae77e108d438bf4e9e3
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-29 09:40:14 +0800

    feat: improve markup and update colors to violet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { cn, formatDate, getAchievements } from "@/lib/utils";
+import { formatDate, getAchievements } from "@/lib/utils";
 
 export default async function Page() {
   const achievements = await getAchievements();
@@ -7,24 +7,24 @@ export default async function Page() {
     <div className="mx-auto max-w-3xl p-4">
       <ol className="">
         {achievements.map((achievement, index) => (
-          <li
-            className={cn(
-              "relative grid grid-cols-1 items-baseline gap-2 pl-10 not-last:pb-6 md:grid-cols-3 md:gap-4",
-              "before:absolute before:top-0.5 before:left-2.5 before:size-4 before:rounded-full before:bg-gray-200 md:before:top-2.5",
-              "after:absolute after:top-1 after:bottom-0 after:left-4.5 after:w-px after:bg-gray-200 md:after:top-0",
-            )}
-            key={`achievement-${index + 1}`}
-          >
-            <div className="md:col-span-1">
-              <h2 className="text-sm text-gray-600">
-                <time className="slashed-zero" dateTime={achievement.title}>
-                  {formatDate(achievement.title)}
-                </time>
-              </h2>
+          <li className="relative flex gap-3" key={`achievement-${index + 1}`}>
+            <div className="flex flex-col items-center pt-1 md:pt-1.5">
+              <div className="size-4 shrink-0 rounded-full bg-violet-500" />
+              <div className="h-full w-0.5 bg-violet-100" />
             </div>
 
-            <div className="prose md:col-span-2 dark:prose-invert prose-h3:tracking-tight">
-              {achievement.content}
+            <div className="grid grid-cols-1 gap-3 pb-6 max-md:pt-0.5 md:w-full md:grid-cols-3">
+              <div className="md:col-span-1 md:pt-1">
+                <h2 className="text-sm text-gray-600">
+                  <time className="slashed-zero" dateTime={achievement.title}>
+                    {formatDate(achievement.title)}
+                  </time>
+                </h2>
+              </div>
+
+              <div className="prose md:col-span-2 dark:prose-invert prose-h3:tracking-tight">
+                {achievement.content}
+              </div>
             </div>
           </li>
         ))}
