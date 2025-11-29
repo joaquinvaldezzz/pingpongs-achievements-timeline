6a1b195a4fe4db9e4351560e28e38f63141c58a6
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-29 22:20:36 +0800

    feat: create providers.tsx

---
+++ src/app/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { RouterProvider } from "react-aria-components";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+declare module "react-aria-components" {
+  interface RouterConfig {
+    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>["push"]>[1]>;
+  }
+}
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <RouterProvider navigate={router.push}>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </RouterProvider>
+  );
+}
