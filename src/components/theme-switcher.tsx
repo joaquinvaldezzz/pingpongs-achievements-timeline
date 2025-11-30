6442315a65fda663c3c0183dfc3d2a23031e420b
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-11-30 16:24:58 +0800

    fix: fix not displaying the right icon

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { ComputerDesktopIcon, MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { useTheme } from "next-themes";
 
@@ -10,16 +11,19 @@ export function ThemeSwitcher({
   ...props
 }: React.ComponentProps<typeof Button>) {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const toggleTheme = () => {
-    let nextTheme: string;
-    if (theme === "light") {
-      nextTheme = "dark";
-    } else if (theme === "dark") {
-      nextTheme = "system";
-    } else {
-      nextTheme = "light";
-    }
+    const themeMap: Record<string, string> = {
+      light: "dark",
+      dark: "system",
+      system: "light",
+    };
+    const nextTheme = themeMap[theme ?? "system"] || "light";
     setTheme(nextTheme);
   };
 
@@ -36,7 +40,7 @@ export function ThemeSwitcher({
 
   return (
     <Button aria-label="Switch theme" intent={intent} size="sq-sm" onPress={toggleTheme} {...props}>
-      {getThemeIcon()}
+      {mounted ? getThemeIcon() : <ComputerDesktopIcon />}
     </Button>
   );
 }
